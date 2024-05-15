c56e33c72525adb74a07221766a231a2c606d0a0
Priyan <priyadharshanr36@gmail.com>
2024-05-15 20:50:32 +0530

    class/object

---
+++ app.py
@@ -0,0 +1,6 @@
+from new import std
+std1 = std("Jim","Sci-Fi",56,False)
+std2 = std("win","Social",3,True)
+
+print(std2.major)
+
---
+++ new.py
@@ -0,0 +1,7 @@
+class Std:
+    def __init__(self, name,major,gpa,is_intern):
+        self.name=name
+        self.major=major
+        self.gpa=gpa
+        self.is_intern=is_intern
+        
